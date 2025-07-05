feb50cbeb4c937dc121d383742d23d09de3b9c4f
Shashank-mankala1 <shashankmankala.5@gmail.com>
2025-07-05 15:41:55 +0530

    Updated NLTK

--- backend/embedder.py
+++ backend/embedder.py
@@ -1,22 +1,13 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
 from langchain_huggingface import HuggingFaceEmbeddings  
 from langchain_community.vectorstores import FAISS
-
 from nltk.tokenize import sent_tokenize
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import AgglomerativeClustering
 from collections import defaultdict
-import numpy as np
 
 def adaptive_chunk_text(text, sentences_per_chunk=5):
-    from nltk.tokenize import sent_tokenize
-    from sentence_transformers import SentenceTransformer
-    from sklearn.cluster import AgglomerativeClustering
-    from collections import defaultdict
-    import numpy as np
-
-    sentences = sent_tokenize(text)
+    sentences = sent_tokenize(text, language="english")
     if len(sentences) < 2:
         return [text]
 
@@ -47,11 +38,7 @@ def chunk_text(text, chunk_size=500, chunk_overlap=50):
     chunks = splitter.split_text(text)
     return chunks
 
-# 2. Create and save FAISS vector store from chunks
 def create_faiss_index(chunks):
-    from langchain_community.vectorstores import FAISS
-    from langchain_huggingface import HuggingFaceEmbeddings
-
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
     texts = [doc.page_content for doc in chunks]
